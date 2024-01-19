f8ac6015dead140b0c7ff2799ea7d34a5d1634a9
Kihsomray <zerotoil.network@gmail.com>
2024-01-18 23:53:43 -0800

    Added tile loading & reorganized

--- src/main.js
+++ src/main.js
@@ -2,7 +2,48 @@ const engine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+const biome = [
+	"cave",
+	"desert",
+	"forest",
+	"swamp",
+	"taiga",
+	"tundra"
+];
+
+const type = [
+	"bridgeAnimatedTiles",
+	"bridgeHorizontal",
+	"bridgeVertical",
+	"fencesAndWalls",
+	"fountain",
+	"resources"
+];
+
+const extra = [
+	["cave", "railway"],
+	["desert", "path"],
+	["desert", "quickSand"],
+	["swamp", "puddle"],
+	["taiga", "path"],
+	["tundra", "ice"],
+	["tundra", "igloo"],
+	["tundra", "path"]
+];
+
+
 ASSET_MANAGER.queueDownload("e/cat", "./res/cat.png");
+
+for (let i = 0; i < biome.length; i++) {
+	for (let j = 0; j < type.length; j++) {
+		ASSET_MANAGER.queueDownload(`t/${biome[i]}/${type[j]}`, `./res/tile/${biome[i]}_/${biome[i]}_ [${type[j]}].png`);
+	}
+	ASSET_MANAGER.queueDownload(`t/${biome[i]}}`, `./res/tile/${biome[i]}_/${biome[i]}_.png`);
+}
+
+for (let i = 0; i < extra.length; i++) {
+	ASSET_MANAGER.queueDownload(`t/${extra[i][0]}/${extra[i][1]}`, `./res/tile/${extra[i][0]}_/${extra[i][1]}_.png`)
+}
 
 ASSET_MANAGER.downloadAll(() => {
 	
