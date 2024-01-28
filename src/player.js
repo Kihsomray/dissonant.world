a528a790aafa0a647c11c389c953e59e538d87c3
Patrick Hern <phern6@yahoo.com>
2024-01-28 15:39:42 -0800

    Updated tick.js for FPS, started work playing proper animations on movement

--- src/player.js
+++ src/player.js
@@ -3,26 +3,29 @@ class PlayerCharacter {
 
         ENGINE.PlayerCharacter = this;
 
-        this.animator = new Animator(ASSET_MANAGER.getImage("e/player"), 0, 0, 24, 24, 4, 0.33, 1, true, true);
+        this.ENGINE = ENGINE;
+
         this.spritesheet = ASSET_MANAGER.getImage("e/player");
 
         // Initial Variables for player's state.
-        this.x = 25;
-        this.y = 150;
+        this.x = 960 / 4 - 16;
+        this.y = 540 / 4 - 24;
         this.speed = 0;
         this.counter = 0;
         this.pause = false;
 
         // State variables.
         this.facing = 0;    // 0 = right, 1 = left.
-        this.state = 0;     // 0 = idle, , walk, turn, damaged, dead.
+        this.state = 0;     // 0 = idle, 1 = walk, run = 2, turn = 3, damaged = 4, dead = 5.
 
         // All of the player's animations.
         this.animations = [];
         this.loadAnimations();
+
     }
 
     loadAnimations() {
+
         for (var i = 0; i < 6; i++) { // 6 total states for player.
             this.animations[i] = [];
             for (var j = 0; j < 2; j++) { // Two directions
@@ -54,7 +57,6 @@ class PlayerCharacter {
         // Turning animation for state = 3.
         // Facing right = 0.
         this.animations[3][0] = new Animator(this.spritesheet, 0, 72, 24, 24, 4, 0.1, 1, false, true)
-
         // Facing left = 1.
         this.animations[3][1] = new Animator(this.spritesheet, 96, 72, 24, 24, 4, 0.1, 1, false, true)
 
@@ -62,7 +64,6 @@ class PlayerCharacter {
         // Player damaged animation for state = 4.
         // Facing right = 0.
         this.animations[4][0] = new Animator(this.spritesheet, 0, 96, 24, 24, 4, 0.2, 1, false, true)
-
         // Facing left = 1.
         this.animations[4][1] = new Animator(this.spritesheet, 96, 96, 24, 24, 4, 0.2, 1, false, true)
 
@@ -70,37 +71,52 @@ class PlayerCharacter {
         // Player death animation for state = 5.
         // Facing right = 0.
         this.animations[5][0] = new Animator(this.spritesheet, 0, 120, 24, 24, 4, 0.33, 1, false, true)
-
         // Facing left = 1.
         this.animations[5][1] = new Animator(this.spritesheet, 96, 120, 24, 24, 4, 0.33, 1, false, true)
+
     }
 
     update() {
+
         if (this.counter++ % 10 == 0) this.pause = !this.pause;
         const location = ENGINE.clockTick * (this.speed + (this.pause ? 0 : 0));
         this.x += location;
         if (this.x > 1024) this.x = -200;
+
+        if (this.ENGINE.right) {
+            this.facing = 0;
+        }
+        if (this.ENGINE.left) {
+            this.facing = 1;
+        }
+
     }
 
     draw(context) {
-        //this.animator.drawFrame(ENGINE.clockTick, context, this.x, this.y, 1.5);
+
+        /*
+         * Movement Legend:
+         * [0][0] = Idle Right      [0][1] = Idle left
+         * [1][0] = Walk Right      [1][1] = Walk Left
+         * [2][0] = Run Right       [2][1] = Run left
+         * [3][0] = Turn Right      [3][1] = Turn Left
+         * [4][0] = Damaged Right   [4][1] = Damaged left
+         * [5][0] = Dead Right      [5][1] = Dead Left
+         */
+
+        // Temp
         this.animations[0][0].drawFrame(ENGINE.clockTick, context, this.x, this.y, 1.5);
-        this.animations[0][1].drawFrame(ENGINE.clockTick, context, this.x + 25, this.y, 1.5);
 
-        this.animations[1][0].drawFrame(ENGINE.clockTick, context, this.x + 75, this.y, 1.5);
-        this.animations[1][1].drawFrame(ENGINE.clockTick, context, this.x + 100, this.y, 1.5);
-        
-        this.animations[2][0].drawFrame(ENGINE.clockTick, context, this.x + 150, this.y, 1.5);
-        this.animations[2][1].drawFrame(ENGINE.clockTick, context, this.x + 175, this.y, 1.5);
+        // IN PROGRESS, WORKING ON GETTING LOGIC RIGHT
+        // if (this.facing == 0 && this.ENGINE.entities[1].speed == 0) { // Idle right
+        //     this.animations[0][0].drawFrame(ENGINE.clockTick, context, this.x, this.y, 1.5);
+        //     console.log("Reached 1");
+        // }
+        // else if (this.facing == 1 && this.ENGINE.entities[1].speed == 0) { // Idle left
+        //     this.animations[0][1].drawFrame(ENGINE.clockTick, context, this.x, this.y, 1.5);
+        //     console.log("Reached 3");
+        // }
 
-        this.animations[3][0].drawFrame(ENGINE.clockTick, context, this.x + 225, this.y, 1.5);
-        this.animations[3][1].drawFrame(ENGINE.clockTick, context, this.x + 250, this.y, 1.5);
-
-        this.animations[4][0].drawFrame(ENGINE.clockTick, context, this.x + 300, this.y, 1.5);
-        this.animations[4][1].drawFrame(ENGINE.clockTick, context, this.x + 325, this.y, 1.5);
-
-        this.animations[5][0].drawFrame(ENGINE.clockTick, context, this.x + 375, this.y, 1.5);
-        this.animations[5][1].drawFrame(ENGINE.clockTick, context, this.x + 400, this.y, 1.5);
     }
 
 }
--- src/util/timer.js
+++ src/util/timer.js
@@ -5,15 +5,31 @@ class Timer {
     constructor() {
         this.gameTime = 0;
         this.lastTimestamp = 0;
+        this.ticks = [];
     };
 
     tick() {
+        /* STATEMENT BELOW DRAWS THE FPS COUNTER:
+            context.fillText(`FPS ${this.gameTime.timer.ticks.length}`, 12.5, 2.5);
+        */
         const current = Date.now();
         const delta = (current - this.lastTimestamp) / 1000;
         this.lastTimestamp = current;
 
         const gameDelta = Math.min(delta, Timer.#maxStep);
         this.gameTime += gameDelta;
+
+        this.ticks.push(delta);
+        
+        let index = this.ticks.length - 1;
+        let sum = 0;
+        while (sum <= 1 && index >= 0) {
+            sum += this.ticks[index--];
+        }
+        index++;
+
+        this.ticks.splice(0, index);
+
         return gameDelta;
     };
 };
