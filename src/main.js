f4911d32b52908e0a76c50bf1d7233a41c987387
Patrick Hern <phern6@yahoo.com>
2024-02-18 20:08:22 -0800

    Added iframes to enemies. Going to merge with main.

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ ASSETS.queueDownload("e/daemon", "./res/daemon.png");
 
 ASSETS.downloadAll(() => {
 
-	// GAME.addEntity(new Enemy("goblin", 220, 0, 4, 15));
+	GAME.addEntity(new Enemy("goblin", 220, 0, 4, 15));
 	// GAME.addEntity(new Enemy("daemon", 100, 100, 4, 15));
 	// GAME.addEntity(new Enemy("orc", 220, 220, 4, 15));
 	// GAME.addEntity(new Enemy("oni", 120, 110, 4, 15));
